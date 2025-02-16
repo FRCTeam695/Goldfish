3636aee3609c15abb68cee00e433d450f8009c3c
shmurthy7002 <yhtrum7002@gmail.com>
2025-02-16 17:20:58 -0500

    reformatted coralizer subsystem

--- src/main/java/frc/robot/subsystems/Coralizer.java
+++ src/main/java/frc/robot/subsystems/Coralizer.java
@@ -6,23 +6,24 @@ import java.util.function.DoubleSupplier;
 
 import com.revrobotics.spark.SparkLowLevel;
 
-import edu.wpi.first.wpilibj.DigitalInput;
+//import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Coralizer extends SubsystemBase{
-    DigitalInput beamBreak;
+    //DigitalInput beamBreak;
     public SparkMax coralizer;
     public SparkMax intake;
     public Coralizer(){
-        beamBreak = new DigitalInput(0);
-        coralizer = new SparkMax(53, SparkLowLevel.MotorType.kBrushless);
+        //beamBreak = new DigitalInput(0);
+        coralizer = new SparkMax(52, SparkLowLevel.MotorType.kBrushless);
     }
 
     public Command runCoralizer(DoubleSupplier speed) {
         return run(
             () -> {
                 coralizer.set(speed.getAsDouble());
-            });
+            }
+        );
     }
 }
