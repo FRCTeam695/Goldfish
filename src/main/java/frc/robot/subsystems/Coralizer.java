f608876ddf5b0a9e6a4e65054263bca379ab1fc3
amber-zang <amberzhang70@gmail.com>
2025-10-16 20:18:26 -0400

    side car revisions

--- src/main/java/frc/robot/subsystems/Coralizer.java
+++ src/main/java/frc/robot/subsystems/Coralizer.java
@@ -1,68 +1,55 @@
 package frc.robot.subsystems;
 
+
 import static edu.wpi.first.wpilibj2.command.Commands.either;
+
 import java.util.function.DoubleSupplier;
+
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
-import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXSConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFXS;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.MotorArrangementValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.ReverseLimitValue;
+
+import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.networktables.IntegerSubscriber;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+//import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.Constants;
+import frc.robot.subsystems.DuoTalonLift.Heights;
 
-public class Coralizer extends SubsystemBase {
-    // Hardware
-    private DigitalInput beamBreak;
+public class Coralizer extends SubsystemBase{
+    //DigitalInput beamBreak;
     private TalonFXS coralizer;
     private TalonFXS intake;
+    private boolean isSafeToRaiseElevator = true;
+    private boolean hasSeenFirstBreak = true;
+    public Trigger safeToRaiseElevator = new Trigger(()-> isSafeToRaiseElevator);
+    public Trigger seenFirstBreak = new Trigger(()-> hasSeenFirstBreak);
+    public Trigger intakeCurrentBelowThreshold;
+    public Trigger isStalled;
+    public Trigger beamIsMadeDebounced;
+    public Trigger intakeCurrentAboveFive;
+    public Debouncer beambreakDebouncer;
+    public DigitalInput fixedBeambreak;
 
-    // configurations
-    private Slot0Configs coralizerConfig;
-
-    // triggers
-    public boolean isSafeToRaiseElevator;
-    public final Trigger safeToRaiseElevator;
-    private boolean hasSeenFirstBreak;
-    public final Trigger seenFirstBreak;
-
-    // sidecar & network tables
-    public IntegerSubscriber scoringHeight;
-    public NetworkTable sideCarTable;
     private final NetworkTableInstance inst = NetworkTableInstance.getDefault();
 
-    // position and velocity
-    private double maxVelocity;
-    private double encoder;
-
-    public Coralizer() {
-
-        maxVelocity = 0.0;
-
-        sideCarTable = inst.getTable("sidecarTable");
-
-        isSafeToRaiseElevator = true;
-        safeToRaiseElevator = new Trigger(() -> isSafeToRaiseElevator);
-        hasSeenFirstBreak = true;
-        seenFirstBreak = new Trigger(() -> hasSeenFirstBreak);
-
-        coralizerConfig = new Slot0Configs();
-        coralizerConfig.kP = .1; // An error of 1 rotation results in 2.4 V output
-
-        beamBreak = new DigitalInput(9);
+    public Coralizer(){
+        //beamBreak = new DigitalInput(0);
         coralizer = new TalonFXS(52);
-
         intake = new TalonFXS(53);
 
+        
         TalonFXSConfiguration config = new TalonFXSConfiguration();
         config.CurrentLimits.SupplyCurrentLimit = 20.0;
         config.HardwareLimitSwitch.ForwardLimitEnable = false;
@@ -77,183 +64,119 @@ public class Coralizer extends SubsystemBase {
         config.MotorOutput = motorOutputConfigs;
 
         coralizer.getConfigurator().apply(config);
-        coralizer.getConfigurator().apply(coralizerConfig);
-
         intake.getConfigurator().apply(config);
 
-        // coralizer.getPosition().setUpdateFrequency(100);
-        intake.getSupplyCurrent().setUpdateFrequency(50);
-
-        coralizer.setPosition(0.0);
-        coralizer.getSupplyCurrent().setUpdateFrequency(50);
-
-        scoringHeight = sideCarTable.getIntegerTopic("scoringLevel").subscribe(1);
-
+        isStalled = new Trigger(()-> intake.getMotorStallCurrent().getValueAsDouble() > 3);
+        intake.getSupplyCurrent().setUpdateFrequency(20);
+        intakeCurrentBelowThreshold = new Trigger(()-> ((intake.getSupplyCurrent().getValueAsDouble() < 3.0) && (intake.getSupplyCurrent().getValueAsDouble() > 0)));
+        beambreakDebouncer = new Debouncer(0.07);
+        beamIsMadeDebounced = new Trigger(()-> !beambreakDebouncer.calculate(beamBrokenUndebounced()));
+        intakeCurrentAboveFive = new Trigger(()-> (intake.getSupplyCurrent().getValueAsDouble() > 5));
+        fixedBeambreak = new DigitalInput(9);
     }
 
-    public Command setDangerousToRaiseElevator() {
-        return runOnce(() -> isSafeToRaiseElevator = false);
+    public boolean beamBrokenUndebounced(){
+        return  coralizer.getReverseLimit().getValue() == ReverseLimitValue.ClosedToGround;
     }
 
-    public Command setSafeToRaiseElevator() {
-        return runOnce(() -> isSafeToRaiseElevator = true);
+    public boolean beamIsBroken(){
+        //return fixedBeambreak.get();
+        return  !beamIsMadeDebounced.getAsBoolean();
+    }
+    
+    public boolean beamNotBroken(){
+    return !beamIsBroken();
     }
 
-    public Command tuneVoltage() {
-        return runOnce(() -> {
-            intake.setVoltage(0.25);
-            System.out.println("Voltage Set");
+    public Command intake(int scoringHeight){
+        return
+        either(
+                runIntakeAndCoralizer(()-> 0.6).withTimeout(0.03)
+                .andThen(new WaitCommand(0.2-0.03))
+                .andThen(runIntakeAndCoralizer(()-> 0.6).until(this::beamIsBroken)
+                .andThen(runIntakeAndCoralizer(()-> -0.1).withTimeout(1.1))
+                .andThen(runIntakeAndCoralizer(()-> 0.0))
+            ),
+            (runIntakeAndCoralizer(()-> 0.6).until(this::beamIsBroken)
+            .andThen(setFirstBreakStateTrue())
+            .andThen(          
+                  runIntakeAndCoralizer(()->0.4).until(intakeCurrentBelowThreshold.and(this::beamNotBroken))
+                  .andThen(setSafeToRaiseElevator())
+                  .andThen(runIntakeAndCoralizer(()-> -0.1).until(this::beamBrokenUndebounced))
+                  .andThen(runIntakeAndCoralizer(()-> 0))
+            )),
+            ()-> scoringHeight == 1).withName("intake");
+    }
+    
+
+    public Command runIntakeAndCoralizer(DoubleSupplier speed){
+        DutyCycleOut output = new DutyCycleOut(0);
+        return run(()->{
+            output.Output = speed.getAsDouble();
+
+            coralizer.setControl(output);
+            intake.setControl(output);
         });
     }
 
-
-    public Command runIndexerInwardUntilCoralizerEncoderDetectsCoral() {
-        return runOnce(() -> {
-            
-            encoder = coralizer.getPosition().getValueAsDouble();
-            intake.setVoltage(0.25);
-            intake.set(1.0); //.5
-
-        }).andThen(run(
-            () -> {})
-        ).until(
-            () -> (coralizer.getPosition().getValueAsDouble() - encoder >= 1.0)
-
-        ).finallyDo( 
-            () -> {
-                intake.set(0.0);
-                coralizer.set(0.0);
-                coralizer.setPosition(0.0);
-            }
-        );
-    }
-
-    public Command advanceCoralOntoElevatorUntilCoralizerDetectsPositionChange() {
-        return runOnce(() -> {
-            coralizer.setPosition(0.0);
-            intake.setVoltage(0.25);
-            intake.set(0.2); //.2
-            coralizer.set(0.2);
-
-        }).andThen(run(() -> {})
-
-        ).until(() -> 
-            (coralizer.getPosition().getValueAsDouble() >= 19.5) && (!beamBreak.get())
-
-        ).finallyDo(() -> {
-            coralizer.set(0.0);
-            intake.set(0.0);
-            coralizer.setPosition(0.0);
-        });
-    }
-
-    public Command rollbackUntilCoralIsNotTooFarOut(){
-        return runOnce(()-> {
-            coralizer.setPosition(0.0);
-            coralizer.set(-0.2);
-        }).andThen(run(() -> {})
-        ).until(() -> 
-            (coralizer.getPosition().getValueAsDouble() <= -2.0) || (beamBreak.get())
-        ).finallyDo(() -> {
-            coralizer.set(0.0);
-            coralizer.setPosition(0.0);
-        });
-    }
-
-    public Command L1Scoring() {
-        return runOnce(
-            () -> {
-                maxVelocity = 0.0;
-                intake.set(0.4);
-                coralizer.set(0.4);
-            }
-        ).andThen(run(
-            () -> {
-                maxVelocity = Math.max(maxVelocity, intake.getVelocity().getValueAsDouble());
-            }
-        )).until(
-            () -> (intake.getVelocity().getValueAsDouble() < (maxVelocity - 3.0))
-        ).finallyDo(
-            () -> {
-                coralizer.set(0);
-                intake.set(0);
-                coralizer.setPosition(0);
-            }
-        );
-    }
-
-    public Command runIntakeMotor(DoubleSupplier speed) {
-        return run(() -> {
-            intake.set(speed.getAsDouble());
+    public Command runIntakeMotor(DoubleSupplier speed){
+        DutyCycleOut output = new DutyCycleOut(0);
+        return run(()->{
+            output.Output = speed.getAsDouble();
+            intake.setControl(output);
         });
     }
 
     public Command runCoralizer(DoubleSupplier speed) {
         return run(
-                () -> {
-                    coralizer.set(speed.getAsDouble());
-                });
+            () -> {
+                coralizer.set(speed.getAsDouble());
+                intake.set(0);
+            }
+        );
     }
 
-    public Command runIntakeAndCoralizer(DoubleSupplier speed) {
-        return run(() -> {
-            coralizer.set(speed.getAsDouble());
-            intake.set(speed.getAsDouble());
+    public Command ejectCoral(){
+        return runCoralizer(()-> 0.6).withTimeout(0.2).andThen(setDangerousToRaiseElevator()).andThen(setFirstBreakStateFalse());
+    }
+
+    public Command fastEjectCoral(){
+        return runCoralizer(()-> 1).withTimeout(0.3).andThen(setDangerousToRaiseElevator()).andThen(setFirstBreakStateFalse());
+    }
+
+    public Command setDangerousToRaiseElevator(){
+        return runOnce(()-> isSafeToRaiseElevator = false);
+    }
+
+    public Command setSafeToRaiseElevator(){
+        return runOnce(()-> {
+            isSafeToRaiseElevator = true;
         });
     }
 
-    public Command runIntakeAndCoralizerNoStop(DoubleSupplier speed) {
-        return new FunctionalCommand(
-                () -> {
-                    coralizer.set(speed.getAsDouble());
-                    intake.set(speed.getAsDouble());
-                },
-                () -> {
-                },
-                interrupted -> {
-                    coralizer.set(0);
-                    intake.set(0);
-                },
-                () -> {
-                    return false;
-                },
-                this);
-    }
-
-    public Command setFirstBreakStateTrue() {
-        return runOnce(() -> {
+    public Command setFirstBreakStateTrue(){
+        return runOnce(()-> {
             hasSeenFirstBreak = true;
         });
     }
 
-    public Command setFirstBreakStateFalse() {
-        return runOnce(() -> hasSeenFirstBreak = false);
+    public Command setFirstBreakStateFalse(){
+        return runOnce(()-> hasSeenFirstBreak = false);
     }
 
-    public Command intake() {
-        return either(
-                L1Scoring(),
-                runIndexerInwardUntilCoralizerEncoderDetectsCoral()
-                        .andThen(advanceCoralOntoElevatorUntilCoralizerDetectsPositionChange()).andThen(rollbackUntilCoralIsNotTooFarOut()).andThen(setSafeToRaiseElevator()),
-                () -> (int) Math.round(scoringHeight.get(Constants.Coralizer.scoringHeightDefault)) == 1)
-                .withName("intake");
-
-    }
-
-    public Command ejectCoral() {
-        return runCoralizer(() -> 0.6).withTimeout(0.2).andThen(setDangerousToRaiseElevator())
-                .andThen(setFirstBreakStateFalse());
+    public Command requireSubsystem(){
+        return new WaitCommand(0);
     }
 
     @Override
-    public void periodic() {
-        
-        SmartDashboard.putNumber("max intake velocity", maxVelocity);
-        SmartDashboard.putNumber("intake velocity", intake.getVelocity().getValueAsDouble());
+    public void periodic(){
+        SmartDashboard.putBoolean("Beambreak", beamIsBroken());
+        SmartDashboard.putBoolean("Beambreak No Debounce", beamBrokenUndebounced());
+        SmartDashboard.putBoolean("Has Finished Intaking", safeToRaiseElevator.getAsBoolean());
+        SmartDashboard.putBoolean("Has Seen First Break", hasSeenFirstBreak);
         SmartDashboard.putNumber("Coralizer speed", coralizer.getVelocity().getValueAsDouble());
         SmartDashboard.putNumber("Intake current", intake.getSupplyCurrent().getValueAsDouble());
         SmartDashboard.putNumber("Coralizer current", coralizer.getSupplyCurrent().getValueAsDouble());
-        SmartDashboard.putBoolean("Safe to Raise elevator", safeToRaiseElevator.getAsBoolean());
-
+        SmartDashboard.putBoolean("Intake Current Above Five", intakeCurrentAboveFive.getAsBoolean());
     }
 }
